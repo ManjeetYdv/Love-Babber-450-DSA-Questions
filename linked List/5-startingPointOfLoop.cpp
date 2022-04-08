4aad08689f5e0a195133cd46246b4e5a80b47da0
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-04-08 18:09:19 +0530

    Create 5-startingPointOfLoop.cpp

---
+++ linked List/5-startingPointOfLoop.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+
+    public :
+    int data;
+    node* next;
+
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+
+};
+
+node* hasLoop(node* head){
+   
+   node* fast = head;
+   node* slow = head;
+   
+
+   while(fast!=NULL and fast->next!=NULL){
+       slow = slow->next;
+       fast = fast->next->next;
+
+       if(slow==fast){
+           return slow;
+       }
+   }
+   return NULL;
+
+}
+
+node* detectFirstLoop(node* head){
+    node* meet = hasLoop(head);
+    node* start = head;
+
+    while(start!=meet){
+        start = start->next;
+        meet = meet->next;
+
+    }
+
+    return start;
+}
+
+void display(node *head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+  
+int main(){
+    node* head = new node(10);
+    head->next = new node(20);
+    head->next->next = new node(30);
+    head->next->next->next = new node(40);
+    head->next->next->next->next = new node(50);
+    head->next->next->next->next = head->next->next->next;
+
+    node* startLoop = detectFirstLoop(head);
+    cout<<startLoop->data;
+
+
+
+
+   
+
+
+
+}
